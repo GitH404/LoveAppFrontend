a91ccbcc7b26455b475e010134ef84c460439907
Hongyu Wang <FKBDNHG@163.com>
2026-07-20 01:01:04 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   publicDir: 'images',
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    strictPort: true,
+  },
 })
